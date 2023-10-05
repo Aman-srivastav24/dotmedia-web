28e136d7b147076afd9bb09d5ab421ee9e5a01dc
Aman Srivastav <shinchanmusic24@gmail.com>
2023-10-05 12:23:07 +0530

    completed show comment page

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,158 +1,216 @@
-import React ,{useEffect,useState}from 'react'
+import React, { useEffect, useState } from 'react'
 import Footer from '../Footer'
-import axios from'axios';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom'
 import { FaHeart } from 'react-icons/fa';
+import {MdCancelPresentation } from 'react-icons/md';
+
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,1,0" />
 function Home() {
   const navigate = useNavigate();
-  const [data , setData] = useState([])
-  const [comment , setComment] = useState("")
+  const [data, setData] = useState([])
+  const [comment, setComment] = useState("")
   useEffect(() => {
-   const token = localStorage.getItem("jwt");
-   if(!token){
-     navigate("/signup")
-   }
-   
-   //fetching all posts
+    const token = localStorage.getItem("jwt");
+    if (!token) {
+      navigate("/signup")
+    }
 
-   axios.get("http://localhost:3000/allposts",{
-   headers:{
-    
+    //fetching all posts
 
-    "Authorization": "Bearer" + localStorage.getItem("jwt")
+    axios.get("http://localhost:3000/allposts", {
+      headers: {
 
-   }}
-    ).then((res)=>{
-    setData(res.data);
-    console.log(res.data)
-    }).catch((err)=>{
-   console.log(err)
-    })
-    }, [])
-  
 
-   //like post
-   const likePost =(id)=>{
-    axios.put("http://localhost:3000/like",{
-      postId:id
-    },{method:"put",
-    headers:
-    {
-      "Content-Type":"application/json",
-      "Authorization": "Bearer" + localStorage.getItem("jwt")
-   },
-  }).then((res)=>{
-     
-    const newData  = data.map((posts)=>{
-      if(posts._id === res.data._id){
-        return res.data
-      }else{
-        return posts
+        "Authorization": "Bearer" + localStorage.getItem("jwt")
+
       }
+    }
+    ).then((res) => {
+      setData(res.data);
+      console.log(res.data)
+    }).catch((err) => {
+      console.log(err)
     })
-    console.log(newData)
-    setData(newData)
-    console.log(res);
-  })}
-  //unlike
-   const unlikePost =(id)=>{
-    axios.put("http://localhost:3000/unlike",{
-      postId:id
-    },{method:"put",
-    headers:
-    {
-      "Content-Type":"application/json",
-      "Authorization": "Bearer" + localStorage.getItem("jwt")
-   },
-  }).then((res)=>{
-    const newData  = data.map((posts)=>{
-      if(posts._id === res.data._id){
-        return res.data
-      }else{
-        return posts
-      }
-    })
-    setData(newData)
-    console.log(newData);
-  })}
-    // function for comment
+  }, [])
 
-    const makeComment = (text,id)=>{
-      axios.put("http://localhost:3000/comments",{
-        text: text,
-      postId:id,
-    },{method:"put",
-    headers:
-    {
-      "Content-Type":"application/json",
-      "Authorization": "Bearer" + localStorage.getItem("jwt")
-   },
-  }).then((res)=>{
-     console.log(comment)
-  
-    console.log(res);
+
+  //like post
+  const likePost = (id) => {
+    axios.put("http://localhost:3000/like", {
+      postId: id
+    }, {
+      method: "put",
+      headers:
+      {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer" + localStorage.getItem("jwt")
+      },
+    }).then((res) => {
+
+      const newData = data.map((posts) => {
+        if (posts._id === res.data._id) {
+          return res.data
+        } else {
+          return posts
+        }
+      })
+      console.log(newData)
+      setData(newData)
+      console.log(res);
+    })
   }
-    )}
-   return (
+  //unlike
+  const unlikePost = (id) => {
+    axios.put("http://localhost:3000/unlike", {
+      postId: id
+    }, {
+      method: "put",
+      headers:
+      {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer" + localStorage.getItem("jwt")
+      },
+    }).then((res) => {
+      const newData = data.map((posts) => {
+        if (posts._id === res.data._id) {
+          return res.data
+        } else {
+          return posts
+        }
+      })
+      setData(newData)
+      console.log(newData);
+    })
+  }
+  // function for comment
+
+  const makeComment = (text, id) => {
+    axios.put("http://localhost:3000/comments", {
+      text: text,
+      postId: id,
+    }, {
+      method: "put",
+      headers:
+      {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer" + localStorage.getItem("jwt")
+      },
+    }).then((res) => {
+      console.log(comment)
+
+      console.log(res);
+    }
+    )
+  }
+  return (
     <div className='flex flex-col justify-center items-center
     bg-black mt-4'>
-      {data.map((posts)=>{
-        return(
+      {data.map((posts) => {
+        return (
           <>
-           <div className='flex flex-col justify-center items-center w-screen h-screen
+            <div className='flex flex-col justify-center items-center w-screen h-screen
     bg-black mt-4'>
-          <div className='flex text-white w-[335px] md:w-[680px] h-[60px]  items-center gap-2'>
-        <img src="https://cdn.pixabay.com/photo/2019/05/04/15/24/woman-4178302_1280.jpg" className='rounded-full w-[40px] h-[40px] ' alt="" srcset="" />
-        <p className='text-[14px]'>{posts.postedBy.userName}<span className='text-gray-500' >&#183; 1d</span></p>
-      </div>
-      <div className='flex  text-white justify-center md:border-[.1px]  w-[335px] md:w-[680px] mt-2 h-[70%] rounded-lg'>
-        <img src={posts.photo} className='' alt="" srcset="" />
-      </div>
-      {/* comment */}
-      <div className='w-[335px] md:w-[680px] flex flex-col  h-[150px] '>
-        <div className='flex gap-3  px-2 mt-2'>
-          {posts.likes.includes(JSON.parse(localStorage.getItem("user"))._id)?(
-            <FaHeart color="red" size={22} onClick={()=>unlikePost(posts._id)} />
-          ):<span className="material-symbols-outlined text-white cursor-pointer  hover:text-red-500 hover:scale-110" onClick={()=>likePost(posts._id)}>
-          favorite
-        </span>}
-          
-          
-          <span className="material-symbols-outlined text-white">
-            chat_bubble
-          </span>
-          <span class="material-symbols-outlined text-white">
-            share
-          </span>
+              <div className='flex text-white w-[335px] md:w-[680px] h-[60px]  items-center gap-2'>
+                <img src="https://cdn.pixabay.com/photo/2019/05/04/15/24/woman-4178302_1280.jpg" className='rounded-full w-[40px] h-[40px] ' alt="" srcset="" />
+                <p className='text-[14px]'>{posts.postedBy.userName}<span className='text-gray-500' >&#183; 1d</span></p>
+              </div>
+              <div className='flex  text-white justify-center md:border-[.1px]  w-[335px] md:w-[680px] mt-2 h-[70%] rounded-lg'>
+                <img src={posts.photo} className='' alt="" srcset="" />
+              </div>
+              {/* comment */}
+              <div className='w-[335px] md:w-[680px] flex flex-col  h-[150px] '>
+                <div className='flex gap-3  px-2 mt-2'>
+                  {posts.likes.includes(JSON.parse(localStorage.getItem("user"))._id) ? (
+                    <FaHeart color="red" size={22} onClick={() => unlikePost(posts._id)} />
+                  ) : <span className="material-symbols-outlined text-white cursor-pointer  hover:text-red-500 hover:scale-110" onClick={() => likePost(posts._id)}>
+                    favorite
+                  </span>}
 
-        </div>
-      <div className='text-white flex flex-col text-[12px]  mt-2'>
-        <span className='flex px-2'>{posts.likes.length} likes</span>
-        <p className='flex px-2 font-bold'>{posts.postedBy.userName}</p>
-        <span className='flex px-2'>{posts.body}</span>
-        {/* comment section */}
-        <p className='flex px-2 text-gray-400'>Show all comments</p>
-        <p className='flex justify-between px-2'>
 
-        <input type="text" placeholder="Add a comment..." value={comment} onChange={(e)=>{setComment(e.target.value)}}  className="flex  focus:outline-none bg-black"/>
+                  <span className="material-symbols-outlined text-white">
+                    chat_bubble
+                  </span>
+                  <span class="material-symbols-outlined text-white">
+                    share
+                  </span>
 
-        <button className='text-blue-300 font-bold hover:scale-110' onClick={()=>{makeComment(comment,posts._id)}} >Post</button>
-        </p>
-      </div>
-        </div>
-        </div>
-         
+                </div>
+                <div className='text-white flex flex-col text-[12px]  mt-2'>
+                  <span className='flex px-2'>{posts.likes.length} likes</span>
+                  <p className='flex px-2 font-bold'>{posts.postedBy.userName}</p>
+                  <span className='flex px-2'>{posts.body}</span>
+                  {/* comment section */}
+                  <p className='flex px-2 text-gray-400 cursor-pointer hover:text-gray-200'>Show all comments</p>
+                  <p className='flex justify-between px-2'>
+
+                    <input type="text" placeholder="Add a comment..." value={comment} onChange={(e) => { setComment(e.target.value) }} className="flex  focus:outline-none bg-black" />
+
+                    <button className='text-blue-300 font-bold hover:scale-110' onClick={() => { makeComment(comment, posts._id) }} >Post</button>
+                  </p>
+                </div>
+              </div>
+            </div>
+
           </>
         )
       })}
-    
+      {/* Show Comments */}
       
-        <br />
-        <Footer/>
+      <div className='fixed z-10 inset-0 overflow-y-auto hidden'>
+      <div className="flex items-center justify-center min-h-screen  border-2">
+        <div className="bg-white rounded-lg shadow-md md:w-[780px] w-[380px]  p-4">
+          <div className="modal-header flex flex-col justify-between items-center">
+            <h2 className="text-2xl  flex gap-4 mb-2"><img src="https://cdn.pixabay.com/photo/2019/05/04/15/24/woman-4178302_1280.jpg" className='rounded-full w-[40px] h-[40px] ' alt="" srcset="" />
+                <p className='text-[19px]'>nidhi<span className='text-gray-500' >&#183; 1d</span></p>
+              </h2>
+            <button
+             
+              className="text-gray-400 hover:text-gray-500  text-[30px] focus:outline-none absolute top-[12%] right-[2%]"
+            >
+              &#215;
+            </button>
+            <div className='w-[380px] md:w-[420px] mb-4'>
+              <img src="https://cdn.pixabay.com/photo/2017/01/28/02/24/japan-2014618_640.jpg" alt="" srcset="" />
+              <div className='flex flex-col'>
+              <span className='text-[15px]'>102 likes</span>
+                 
+                 <span className='caption text-[16px] text-gray-500 max-h-20 overflow-y-auto'>Ld tenetur accusamus, ex necessitatibus maiores, ad sunt odio nemo dolores consequuntur adipisci laborum omnis alias.</span>
+                 </div>
+            </div>
+          </div>
+          <div >
+            <div className="comments max-h-40 overflow-y-auto gap-4 flex flex-col">
+              <p className='flex gap-2'>
+                <img src="https://cdn.pixabay.com/photo/2017/01/28/02/24/japan-2014618_640.jpg" className='w-[40px] rounded-full h-[40px]'  alt="" />
+                <span className='font-bold mt-2'>aman_srivastav</span>
+                <span className='mt-2'>Nice shot , Lorem t.</span>
+              </p>
+              
+
+            </div>
+          </div>
+          <div className="modal-footer text-center py-2 border-t">
+          <p className='flex justify-between px-2'>
+              <input
+                type="text"
+                placeholder="Add a comment..."
+                value={comment}
+                onChange={(e) => { setComment(e.target.value) }}
+                className="flex  focus:outline-none "
+              />
+              <button className='text-blue-500 font-bold hover:scale-110 hover:text-blue-700' onClick={() => { makeComment(comment, posts._id) }} >Post</button>
+            </p>
+          </div>
+        </div>
       </div>
-      
-      )
+    </div>
+      <br />
+      <Footer />
+    </div>
+
+  )
 }
 
-      export default Home
+export default Home
+{/*  */}
