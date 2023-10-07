3845d8fe24ac2a8a12efa1b4fa477526914e911a
Aman Srivastav <shinchanmusic24@gmail.com>
2023-10-07 19:07:57 +0530

    change in app.js auth.js path

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,9 @@ const port = process.env.port || 3000;
 import { config } from 'dotenv'
 import cors from'cors';
 import path from 'path'
-import userRouter from '../backend/routes/auth.js';
-import createPostRouter from '../backend/routes/createPost.js'
-import userProfileRouter from '../backend/routes/userprofile.js'
+import userRouter from './routes/auth';
+import createPostRouter from '.routes/createPost.js'
+import userProfileRouter from './routes/userprofile.js'
 app.use(cors());
 
 
